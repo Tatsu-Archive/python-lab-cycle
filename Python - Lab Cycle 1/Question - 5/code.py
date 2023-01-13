b8fb57926f19b74d353475ac429d1512e86f123f
Tatsuya <pruthvikanth007@gmail.com>
2023-01-13 14:51:58 +0530

    questions complete

--- Python - Lab Cycle 1/Question - 5/code.py
+++ Python - Lab Cycle 1/Question - 5/code.py
@@ -19,8 +19,18 @@ def subs_dist_length(string, l, n):
     i, j, list = 0, 0, []
     for i in range(0, len(string)+1):
         for j in range(i+1, len(string)+1):
+            sub = string[i:j]
+            if (len(sub) == l):
+                if len(set(sub)) == n:
+                    list.append(sub)
+    return list
+
+
+def subs_dist_max_length(string, n):
+    i, j, list = 0, 0, []
+    for i in range(0, len(string)+1):
+        for j in range(i+1, len(string)+1):
             s1 = string[i:j]
-            # To make the strings unique that is ton delete duplicate characters
             dist = set(s1)
             if (len(dist) == n):
                 print(s1, end=",")
@@ -30,8 +40,9 @@ def palindrome_subs(string):
     i, j, list = 0, 0, []
     for i in range(0, len(string)+1):
         for j in range(i+1, len(string)+1):
-            if string[i:j] == string[::-1]:
-                list.append(string[i:j])
+            sub = string[i:j]
+            if sub == sub[::-1]:
+                list.append(sub)
     return list
 
 
@@ -59,10 +70,10 @@ while True:
             print(i, end=" ")
 
     elif choice == 3:
-
-        n = int(input("Enter the length of substring: "))
-        print(f"\nSubstrings of length {n} are: ")
-        list = subs_length(string, n)
+        n = int(input("Enter the number of distinct letters: "))
+        l = int(input("Enter the length of substring: "))
+        print(f"\nSubstrings of length {n} and {l} distinct letters are: ")
+        list = subs_dist_length(string, l, n)
         for i in list:
             print(i, end=" ")
 
@@ -74,16 +85,3 @@ while True:
 
     else:
         break
-
-
-# k=int(input("Enter the length of the substrings that you want to print:"))
-# print("The possible substrings with length",k,"is:")
-# subs_with_length(s,k)
-
-# nof_dist=int(input("Enter the number of distinct characters in the substrings"))
-# print("The substrings with",k,"length and",nof_dist,"distinct characters are:")
-# subs_dist_char_and_length(s,k,nof_dist)
-
-
-# print("All the palindrome substrings are:")
-# print_palindrome_subs(s)
