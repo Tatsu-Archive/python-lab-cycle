954f8593863b9341a6e17f549617274a818bc32d
tatsuya <pruthvikanth007@gmail.com>
2023-02-21 14:39:55 +0530

    change

--- Python - Lab Cycle 2/Question - 2/code.py
+++ Python - Lab Cycle 2/Question - 2/code.py
@@ -1,25 +1,16 @@
-string = "1 2 3 4 5 6 7 8 9 10"
-list = [int(n) for n in string.split(" ")]
+'''Write a program to read a string containing numbers separated by a
+space and convert it as a list of integers. Perform the following
+operations on it.
+1. Rotate elements in a list by 'k' position to the right
+2. Convert the list into a tuple using list comprehension
+3. Remove all duplicates from the tuple and convert them
+into a list again.
+4. Create another list by putting the results of the evaluation
+of the function 𝑓(𝑥) = 𝑥
+2 – 𝑥 with each element in the
+final list
+5. After sorting them individually, merge the two lists to
+create a single sorted list.'''
 
-def rotate_right(list,k):
-    for i in range(k):
-        pop = list.pop()
-        list.insert(0,pop)
-    return list
-
-def tuple_list(list):
-    return tuple([x for x in list])
-
-def dup_list(tup):
-    duptup =  tuple(set(tup))
-    return list(duptup)
-
-string = input("Enter the string(num): ")
-list = [int(n) for n in string.split(" ")]
-k = int(input("Enter the no of positions to rotate: "))
-print("\nRotated list: ",rotate_right(list,k))
-
-tup = tuple_list(list)
-print("\nTuple:",tup)
-
-print("\nDuplicates Removed List: ",dup_list(tup))
+def convert_list(string):
+        
